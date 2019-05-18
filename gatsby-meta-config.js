259d6c5675d0f06e0fffc5c8fabe6a96b0e40490
jason <jason0853@gmail.com>
2019-05-18 17:01:01 +0900

    gatsby blog configuration

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `jason.log`,
+  description: `Blog posted about my dev study.`,
+  author: `Jaesung Lee`,
+  introduction: `프론트엔드 개발자`,
+  siteUrl: `https://jason0853.github.io`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    twitter: `js0853`, // Your Twitter account
+    github: `jason0853`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    facebook: `js0853`, // Your Facebook account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [`frontend`, `javascript`, 'typescript'],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'jason0853/gatsby-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'HFzVClS',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-106873845-1', // Add your google analytics tranking ID
 }
