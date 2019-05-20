a485388b652c23d2f1ddcbe4460a09be60fd98f8
jason <jason0853@gmail.com>
2019-05-20 11:10:58 +0900

    Change github id on footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,7 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
+    ©<a href="https://github.com/jason0853">jason0853</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
